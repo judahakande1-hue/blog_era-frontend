676d40306c2ce83fc643cc34bd5ada65b9d60bda
Judah Akande <judahakande1@gmail.com>
2026-06-19 14:51:50 +0100

    remove verify again

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -2,6 +2,7 @@ import { Eye, EyeOff } from "lucide-react";
 import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import Toast from "./Toast";
+import VerifyEmail from "./VerifyEmail"
 
 export default function SignUp() {
   const [showPassword, setShowPassword] = useState(false);
@@ -10,7 +11,7 @@ export default function SignUp() {
   const [email, setEmail] = useState("");
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
-
+  
   const [toast, setToast] = useState({
     type: "",
     message: "",
@@ -50,28 +51,31 @@ export default function SignUp() {
 
       setToast({
         type: "success",
-        message: data.message || "Account created. Please verify your email.",
+        message: data.message || "Account created successfully",
       });
 
-      localStorage.setItem("pendingVerificationEmail", data.email || email);
+      localStorage.setItem("token", data.token);
+      localStorage.setItem("userId", data.user.id || data.user._id);
+      localStorage.setItem("username", data.user.username);
+      localStorage.setItem("email", data.user.email);
+
+      localStorage.setItem("token", data.token);
+      localStorage.setItem("userId", data.user.id || data.user._id);
+      localStorage.setItem("username", data.user.username);
+      localStorage.setItem("email", data.user.email);
 
       setTimeout(() => {
-        navigate("/verify-email", {
-          state: {
-            email: data.email || email,
-          },
-        });
+        navigate("/dashboard");
       }, 1000);
     } catch (error) {
       setToast({
         type: "error",
-        message: "Cannot connect to backend. Try again.",
+        message: `Cannot connect to backend. Try again.${error.message}`,
       });
     } finally {
       setLoading(false);
     }
   }
-
   return (
     <div className="min-h-screen bg-gray-400 pt-24 px-4 flex flex-col items-center">
       <div className="w-full max-w-md lg:max-w-3xl flex flex-col-reverse lg:grid lg:grid-cols-2 mt-6 shadow-2xl rounded-xl overflow-hidden">
@@ -148,6 +152,7 @@ export default function SignUp() {
 
             <button
               type="submit"
+              
               disabled={loading}
               className="w-full sm:w-auto bg-purple-600 text-white mt-3 hover:bg-purple-700 font-bold py-2 px-6 rounded-full disabled:bg-gray-400"
             >
